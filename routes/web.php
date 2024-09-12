bfe33f16ef9812ac6df366c3a1222948057b1859
thiagosilva327@live.com <thiago@Jarvis>
2024-09-12 16:36:19 -0300

    feat: add route and controller with resource for CRUD

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ClientController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -17,4 +18,6 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+Route::resource('client', ClientController::class)->middleware('auth');
+
+require __DIR__ . '/auth.php';
