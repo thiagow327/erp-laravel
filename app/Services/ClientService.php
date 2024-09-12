d9cad1a41ea86499f82abf27aa2ed84c02274ccd
thiagosilva327@live.com <thiago@Jarvis>
2024-09-12 16:57:17 -0300

    feat: add service to clients

---
+++ app/Services/ClientService.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Client;
+
+class ClientService
+{
+    protected $model;
+
+    public function __construct()
+    {
+        $this->model = new Client();
+    }
+
+    public function index()
+    {
+        return $this->model->all();
+    }
+
+    public function create(array $data)
+    {
+        return $this->model->create($data);
+    }
+
+    public function show(string $id)
+    {
+        return $this->model->findOrFail($id);
+    }
+
+    public function update(array $data, string $id)
+    {
+        $client = $this->model->findOrFail($id);
+        $client->update($data);
+
+        return $client;
+    }
+
+    public function destroy(string $id)
+    {
+        $client = $this->model->findOrFail($id);
+        $client->delete();
+
+        return $client;
+    }
+}
